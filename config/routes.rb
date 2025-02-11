b901eb72c2570d720f69552ca0f7eb197682f7e1
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-11 00:42:58 +0000

    dynamic path segment working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get("/", { :controller => "dice", :action => "homepage" })
-  get("dice/2/6", { :controller => "dice", :action => "dynamic_roll" })
+  get("dice/:number/:sides", { :controller => "dice", :action => "dynamic_roll" })
 end
